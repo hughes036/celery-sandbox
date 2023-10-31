048cfb5c1a42c30d815a26245774efa44ac6cb2b
chrishu <chrishu@alleninstitute.org>
2023-10-31 13:51:27 -0400

    aics user

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@dev-aics-tfp-002:80')
+app = Celery('tasks', broker='pyamqp://aics:aics@dev-aics-tfp-002:80')
 
 @app.task
 def add(x, y):
