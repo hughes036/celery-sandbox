d4ff5849365256b723df38a8779356f51e622f49
chrishu <chrishu@alleninstitute.org>
2024-01-08 11:10:48 -0500

    cpu and io bound test cases

--- producer.py
+++ producer.py
@@ -1,10 +1,72 @@
-from tasks import add
+import json
+import time
+from tasks import add, ioBoundTask, cpuBoundTask
 from functools import reduce
 
-responses = []
-for i in range(10):
-    responses.append(add.delay(2, 2))
 
-results = map(lambda r: r.get(), responses)
-sum = reduce(lambda x, y: x + y, results)
-print(sum)
+NUM_JOBS_DEFAULT = 50
+
+
+def testAdd(x, y):
+    responses = []
+    for i in range(NUM_JOBS_DEFAULT):
+        responses.append(add.delay(x, y))
+
+    results = map(lambda r: r.get(), responses)
+    sum = reduce(lambda x, y: x + y, results)
+    print(sum)
+
+
+def testIoBoundTask(num_jobs=NUM_JOBS_DEFAULT):
+    start = time.time()
+    responses = []
+    for _ in range(NUM_JOBS_DEFAULT):
+        responses.append(ioBoundTask.delay())
+
+    results = map(lambda r: r.get(), responses)
+    reduced_result = reduce(
+        _reduce_result,
+        list(results),
+        {}
+    )
+    # print(json.dumps(reduced_result))
+    print(f"{num_jobs} Jobs | Time taken: {time.time() - start}")
+
+
+def testCpuBoundTask(num_jobs=NUM_JOBS_DEFAULT):
+    start = time.time()
+    compute_results = []
+    for _ in range(NUM_JOBS_DEFAULT):
+        compute_results.append(cpuBoundTask.delay(5))
+
+    results = list(map(lambda r: r.get(), compute_results))
+    reduced_result = reduce(
+        _reduce_result,
+        list(results),
+        {}
+    )
+    # print(json.dumps(reduced_result))
+    print(f"{num_jobs} Jobs | Time taken: {time.time() - start}")
+
+
+def _reduce_result(acc, code):
+    if code in acc:
+        acc[code] += 1
+    else:
+        acc[code] = 1
+    return acc
+
+
+def main():
+    # testAdd(2, 3)
+    print("________Testing IO bound task________")
+    # test of n = 1 to negate any caching effects
+    testIoBoundTask(1)
+    for num_jobs in [10, 50, 100, 200]:
+        testIoBoundTask(num_jobs)
+    print("________Testing CPU bound task________")
+    for num_jobs in [10, 50, 100, 200]:
+        testCpuBoundTask(num_jobs)
+
+if __name__ == "__main__":
+    main()
--- tasks.py
+++ tasks.py
@@ -1,17 +1,32 @@
 from celery import Celery
+import time
+import requests
+app = Celery('tasks', broker='pyamqp://guest@dev-aics-tfp-002:8080', backend='rpc://',) # using rabbitMQ instance as backend
 
-app = Celery('tasks', broker='pyamqp://aics:aics@dev-aics-tfp-002:80', backend='rpc://',) # using rabbitMQ instance as backend
+# Configure Celery to use threads for concurrency
+app.conf.update(
+    task_concurrency=4,  # Use 4 threads for concurrency
+    worker_prefetch_multiplier=1  # Prefetch one task at a time
+)
 
 @app.task
 def add(x, y):
     return x + y
 
-
 @app.task
 def mul(x, y):
     return x * y
 
-
 @app.task
 def xsum(numbers):
     return sum(numbers)
+
+@app.task
+def cpuBoundTask(sleep_time=1):
+    time.sleep(sleep_time)
+    return f"slept for {sleep_time} seconds"
+
+@app.task
+def ioBoundTask():
+    x = requests.get('https://w3schools.com')
+    return str(x.status_code)
